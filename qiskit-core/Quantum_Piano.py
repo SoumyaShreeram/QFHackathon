7cb2ea2485fbf9cc463449d9b8e8f4b334cf6216
L-P-B <32333736+L-P-B@users.noreply.github.com>
2019-10-20 00:56:26 +0200

    Add files via upload

--- qiskit-core/Quantum_Piano.py
+++ qiskit-core/Quantum_Piano.py
@@ -1,3 +1,18 @@
+
+## Here we create music
+list_of_input=[]
+
+import pygame
+import datetime
+import time
+from tkinter import *
+
+pygame.init()
+root = Tk()
+root.title("Quantum Music")
+root.geometry('1352x7000+0+0') # open from the upper-left
+root.configure(background='white') # background configuration
+
 import numpy
 import qiskit
 from qiskit import QuantumRegister, ClassicalRegister, QuantumCircuit
@@ -11,55 +26,18 @@ from qiskit.visualization import plot_histogram
 from qiskit.transpiler import PassManager
 from qiskit.transpiler.passes import Unroller
 import json
-import pygame
-import datetime
-import time
-from tkinter import *
+import matplotlib.pyplot as plt
+# import pygame
+# import datetime
+# import time
+# from tkinter import *
 IBMQ.load_account()
 
-# This code is going to impliment all the quantum Algorithms in qiskit and return the final value.
-def frameProperties(root, frameTrue):
-    if frameTrue:
-        frame = Frame(root, bg="#f0c2e0", bd=15, relief=RIDGE)
-        frame.grid()
-    else:
-        frame = Frame(root, bg="#f0c2e0", relief=RIDGE)
-        frame.grid()
-    return frame
+#IBMQ stuff (initialize the IBM account)
+IBMQ.load_account()
+provider = IBMQ.get_provider(group='open')
 
-def labelTitle(input_frame, input_text, color_bg, fontsize):
-    Label(input_frame, text=input_text, font=('arial', fontsize, 'bold'), padx=8, pady=8, bd=34, bg=color_bg, fg="white", justify=CENTER).grid(row=0, column=0, columnspan=11)
-    return
-
-def textEntries(input_frame, input_text, color_bg, fontsize, i, j):
-    Entry(input_frame, textvariable=input_text, font=('arial', fontsize, 'bold'), bd=34, bg=color_bg, fg="white", width=20, justify=CENTER).grid(row=i, column=j, columnspan=11)
-    return
-
-def textButtons(input_frame, input_text, color_bg, fontsize, i, j, k, l, font_color, command_name):
-    butts=Button(input_frame,  height= j, width=i, text=input_text, font=('arial', fontsize, 'bold'), command=command_name, bd=4, bg=color_bg, fg=font_color)
-    butts.grid(row=k, column=l, padx=5, pady=5)
-    return butts
-
-def ValueC():
-    Circuit_Runner('C')
-    return
-
-def ValueCs():
-    Circuit_Runner('C#')
-    return
-
-def ValueD():
-    Circuit_Runner('D')
-    return
-
-def ValueDs():
-    Circuit_Runner('D#')
-    return
-
-def ValueE():
-    Circuit_Runner('E')
-    return
-
+######################### music definitions
 def flat(i, iflat):
     melodyCirc.x(iflat)
     melodyCirc.cx(iflat, i)
@@ -93,21 +71,62 @@ def Melinit(nrq):
 def silence(nrq):
     melodyCirc.I()
     
+
+######################### pygame stuff definitions
+
+# This code is going to impliment all the quantum Algorithms in qiskit and return the final value.
+def frameProperties(root, frameTrue):
+    if frameTrue:
+        frame = Frame(root, bg="#f0c2e0", bd=15, relief=RIDGE)
+        frame.grid()
+    else:
+        frame = Frame(root, bg="#f0c2e0", relief=RIDGE)
+        frame.grid()
+    return frame
+
+def labelTitle(input_frame, input_text, color_bg, fontsize):
+    Label(input_frame, text=input_text, font=('arial', fontsize, 'bold'), padx=8, pady=8, bd=34, bg=color_bg, fg="white", justify=CENTER).grid(row=0, column=0, columnspan=11)
+    return
+
+def textEntries(input_frame, input_text, color_bg, fontsize, i, j):
+    Entry(input_frame, textvariable=input_text, font=('arial', fontsize, 'bold'), bd=34, bg=color_bg, fg="white", width=20, justify=CENTER).grid(row=i, column=j, columnspan=11)
+    return
+
+def textButtons(input_frame, input_text, color_bg, fontsize, i, j, k, l, font_color, command_name):
+    butts=Button(input_frame,  height= j, width=i, text=input_text, font=('arial', fontsize, 'bold'), command=command_name, bd=4, bg=color_bg, fg=font_color)
+    butts.grid(row=k, column=l, padx=5, pady=5)
+    # return butts
+
+def ValueC():
+    Circuit_Runner('C')
+    return
+
+def ValueCs():
+    Circuit_Runner('C#')
+    return
+
+def ValueD():
+    Circuit_Runner('D')
+    return
+
+def ValueDs():
+    Circuit_Runner('D#')
+    return
+
+def ValueE():
+    Circuit_Runner('E')
+    return
+    
+######################### qiskit definitions
 def Circuit_stuff(qc,backend,number_of_keys,shots,q,c):
     qc.barrier(q[0:number_of_keys])
     qc.measure(q[0:number_of_keys], c[0:number_of_keys])
+    plt.figure()
     qc.draw(output='mpl', filename='my_circuit.png')
     job = execute(qc, backend, shots=shots)
     result = job.result()
     count = result.get_counts()
-    return(count)
-
-#IBMQ stuff (initialize the IBM account)
-IBMQ.load_account()
-provider = IBMQ.get_provider(group='open')
-
-## Here we create music
-list_of_input=[]
+    return count
 
 def Circuit_Runner(note):
     # First define our circuit: 
@@ -119,7 +138,9 @@ def Circuit_Runner(note):
     qc = QuantumCircuit(q,c)
     list_of_input.append(note)
     for input_var in list_of_input:
+        print(input_var)
         if input_var == 'C':
+            print('HEllo1')
             qc.x(q[0])
             count = Circuit_stuff(qc,backend,number_of_keys,shots, q,c)
         if input_var == 'C#':
@@ -134,8 +155,9 @@ def Circuit_Runner(note):
         if input_var == 'E':
             qc.x(q[4])
             count = Circuit_stuff(qc,backend,number_of_keys,shots,q,c)
-
-    Volume_Calculations(count,number_of_keys,shots)
+    print('HEllo')
+    #a = Volume_Calculations(count,number_of_keys,shots)
+    #return a
 
 def Volume_Calculations(count,number_of_keys,shots):
     key_results = np.zeros([number_of_keys])
@@ -154,13 +176,6 @@ def Volume_Calculations(count,number_of_keys,shots):
                 key_results[j]+=probabilities[i]
     key_results=key_results/float(shots)
     return(key_results)
-
-pygame.init()
-
-root = Tk()
-root.title("Quantum Music")
-root.geometry('1352x7000+0+0') # open from the upper-left
-root.configure(background='white') # background configuration
 
 # main frame
 main_frame = frameProperties(root, True)
@@ -183,10 +198,13 @@ textEntries(title_frame, str1, "#1a0033", 18, 1, 2)
 keys_sharp = ["C#", "D#"]
 keys = ["C", "D", "E"]
 
-btnC = textButtons(body_frame, "C", "white", 18, 9, 2, 0, 0, "black", ValueC)
+textButtons(body_frame, "C", "white", 18, 9, 2, 0, 0, "black", ValueC)
 btnCs = textButtons(body_frame, "C#", "black", 18, 7, 1, 2, 0, "white", ValueCs)
 bbtnD = textButtons(body_frame, "D", "white", 18, 9, 2, 4, 0, "black", ValueD)
 btnDs = textButtons(body_frame, "D#", "black", 18, 7, 1, 6, 0, "white", ValueDs)
 btnE = textButtons(body_frame, "E", "white", 18, 9, 2, 8, 0, "black", ValueE)
 
 root.mainloop()
+
+
+
